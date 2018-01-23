3a79bc91f92c375b3e45c676daf21778d750ad2c
Sompob Saralamba <sakngoi@hotmail.com>
2018-01-23 10:05:42 +0700

    add dict.path

--- R/swath.R
+++ R/swath.R
@@ -6,7 +6,7 @@
 SwathPath <- file.path(system.file(package='RSwath'),'swath-0.3.4')
 SwathdictPath <- file.path(system.file(package='RSwath'),'swath-0.3.4','swath')
 
-rswath <- function(inputfile, outputfile = NULL, verbose = FALSE, delimitor = NULL, format = NULL, method = NULL) {
+rswath <- function(inputfile, outputfile = NULL, verbose = FALSE, delimitor = NULL, format = NULL, method = NULL, dict.path = NULL) {
   #check the inputs
   if(is.null(inputfile)){
     stop("I can't find your input file.")
@@ -32,19 +32,28 @@ rswath <- function(inputfile, outputfile = NULL, verbose = FALSE, delimitor = NU
     mt<-NULL
   }
 
+  if(is.null(dict.path)){
+    dict.path <- SwathdictPath
+  }
+
   ## if outputfile is NULL then return the output as a vector
   if(is.null(outputfile)){
     tmpdir <- tempdir()
     outputfilename <- paste0(tmpdir,"/rswath.out")  # temp output file
-    cmd<-paste0(SwathPath,'/swath -u u,u',vb,dm,fm,' -d ',SwathdictPath,' < ',inputfile,' > ',outputfilename)
+    cmd<-paste0(SwathPath,'/swath -u u,u',vb,dm,fm,' -d ',dict.path,' < ',inputfile,' > ',outputfilename)
     shell(shQuote(cmd,type="cmd"))
     outtxt<-as.vector(strsplit(readLines(outputfilename,encoding="UTF-8"),"[|]")[[1]])
     outtxt
   }
   else{
-    cmd<-paste0(SwathPath,'/swath -u u,u',vb,dm,fm,' -d ',SwathdictPath,' < ',inputfile,' > ',outputfile)
+    cmd<-paste0(SwathPath,'/swath -u u,u',vb,dm,fm,' -d ',dict.path,' < ',inputfile,' > ',outputfile)
     shell(shQuote(cmd,type="cmd"))
   }
+}
+
+#read the swath outfile as the vector
+SwathToVec <- function(inputfile){
+  as.vector(strsplit(readLines(inputfile),"[|]")[[1]])
 }
 
 
